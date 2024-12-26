5fc5e902ca00309ed4f88f1b16c3770404263afe
Janani Saravana <158731761+janani92004@users.noreply.github.com>
2024-12-26 17:20:41 +0530

    Update medibot.py

--- medibot.py
+++ medibot.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import random
-from IPython.display import Image, display, YouTubeVideo
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
